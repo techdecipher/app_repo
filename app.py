d729ce2a968a1e985744e9bb571ce4f45c41bb38
techdecipher <waytopranav22@gmail.com>
2025-08-05 18:12:49 +0530

    cicd git app

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 from flask import Flask
-
-
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return "Hello from Flask on EKS!"
-
-
-@app.route('/health')
-def health():
-    return "Healthy", 200
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def hello():
+    return "Flask app running from Docker on EC2!"
